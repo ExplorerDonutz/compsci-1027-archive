679d47ce057a9ec1e240cdc3ed684ef35655dc91
ExplorerDonutz <mwquick04@gmail.com>
2023-03-18 15:00:43 -0400

    Completed some DLPriorityQueue methods for assignment 3

--- src/assign3/DLPriorityQueue.java
+++ src/assign3/DLPriorityQueue.java
@@ -1,22 +1,76 @@
 package assign3;
 
-public class DLPriorityQueue<T> implements PriorityQueueADT<T>{
-   private DLinkedNode<T> front;
-   private DLinkedNode<T> rear;
-   private int count;
+/**
+ * A priority queue that holds double values in order of their priority
+ *
+ * @param <T> The data type stored in the nodes
+ * @author Michael Quick
+ * @version 1.0, 2023/03/18
+ */
+public class DLPriorityQueue<T> implements PriorityQueueADT<T> {
+    private DLinkedNode<T> front;
+    private DLinkedNode<T> rear;
+    private int count;
 
-   public DLPriorityQueue() {
-
-   }
-
-    @Override
-    public void add(T data, double priority) {
+    /**
+     * Creates an empty priority queue
+     */
+    public DLPriorityQueue() {
 
     }
 
+    /**
+     * Adds a data item to the priority queue based on its priority
+     *
+     * @param data     item to be added onto the priority queue
+     * @param priority the priority of the data being added
+     */
+    @Override
+    public void add(T data, double priority) {
+        DLinkedNode<T> newNode = new DLinkedNode<>(data, priority);
+        if (front == null) {
+            front = newNode;
+            rear = newNode;
+        } else if (priority <= front.getPriority()) {
+            // The new node has a lower priority than front, so it should go in the front of the queue
+            newNode.setNext(front);
+            front.setPrev(newNode);
+            front = newNode;
+        } else if (priority > rear.getPriority()) {
+            // The new node has a higher priority than rear, so it should go in the rear
+            rear.setNext(newNode);
+            newNode.setPrev(rear.getNext());
+            rear = newNode;
+        } else {
+            // The priority lies somewhere within the middle of the queue
+            DLinkedNode<T> start = front.getNext();
+            while (start.getPriority() > priority) {
+                start = start.getNext();
+            }
+
+            start.getPrev().setNext(newNode);
+            newNode.setNext(start.getPrev());
+            newNode.setPrev(start.getPrev().getNext());
+            start.setPrev(newNode.getNext());
+        }
+    }
+
+    /**
+     * @return The data item removed from the queue
+     * @throws EmptyPriorityQueueException if the priority queue is empty
+     */
     @Override
     public T removeMin() throws EmptyPriorityQueueException {
-        return null;
+        T minData = front.getDataItem();
+
+        // Replace front with the next item in queue
+        front = front.getNext();
+
+        // If front is now null, then the queue is now empty
+        if (front == null)
+            rear = null;
+
+        return minData;
     }
 
     @Override
@@ -26,19 +80,30 @@ public class DLPriorityQueue<T> implements PriorityQueueADT<T>{
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return front == null && rear == null;
     }
 
     @Override
     public int size() {
+        if (front != null) {
+            // Loop through the queue until the last item is found
+            int size = 1;
+            DLinkedNode<T> start = front.getNext();
+            while (start != null) {
+                size++;
+                start = start.getNext();
+            }
+            return size;
+        }
+        // Queue is empty
         return 0;
     }
 
     public String toString() {
-       return "";
+        return "";
     }
 
     public DLinkedNode<T> getRear() {
-       return rear;
+        return rear;
     }
 }
--- src/assign3/DLinkedNode.java
+++ src/assign3/DLinkedNode.java
@@ -1,50 +1,66 @@
 package assign3;
 
-public class DLinkedNode <T>{
+/**
+ * Node for a doubly linked list
+ * @param <T> The data type stored in this node
+ * @author Michael Quick
+ * @version 1.0, 2023/03/18
+ */
+public class DLinkedNode<T> {
     private T dataItem;
     private double priority;
     private DLinkedNode<T> next;
     private DLinkedNode<T> prev;
 
+    /**
+     * Creates a node with a data item and priority
+     * @param data Data item being stored in this node
+     * @param prio The priority of this node
+     */
     public DLinkedNode(T data, double prio) {
         dataItem = data;
         priority = prio;
     }
 
+    /**
+     * Creates an empty node
+     */
     public DLinkedNode() {
         dataItem = null;
         priority = 0;
     }
 
+    // Getters and setters
+
     public double getPriority() {
         return priority;
+    }
+
+    public void setPriority(double priority) {
+        this.priority = priority;
     }
 
     public T getDataItem() {
         return dataItem;
     }
 
-    public DLinkedNode<T> getNext() {
-        return next;
-    }
-
-    public DLinkedNode<T> getPrev() {
-        return prev;
-    }
-
     public void setDataItem(T dataItem) {
         this.dataItem = dataItem;
+    }
+
+    public DLinkedNode<T> getNext() {
+        return next;
     }
 
     public void setNext(DLinkedNode<T> next) {
         this.next = next;
     }
 
-    public void setPrev(DLinkedNode<T> prev) {
-        this.prev = prev;
+    public DLinkedNode<T> getPrev() {
+        return prev;
     }
 
-    public void setPriority(double priority) {
-        this.priority = priority;
+    public void setPrev(DLinkedNode<T> prev) {
+        this.prev = prev;
     }
 }
