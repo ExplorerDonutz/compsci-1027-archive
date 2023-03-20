7c20354b1bec727b0d190ab74a865dae4cf96c7d
ExplorerDonutz <mwquick04@gmail.com>
2023-03-20 01:01:12 -0400

    Working on FindShortestPath for assign 3

--- src/assign3/FindShortestPath.java
+++ src/assign3/FindShortestPath.java
@@ -7,10 +7,28 @@ public class FindShortestPath {
         try {
             Dungeon dungeon = new Dungeon(args[0]);
             DLPriorityQueue<Hexagon> priorityQueue = new DLPriorityQueue<>();
-            Hexagon start = dungeon.getStart();
+            Hexagon hex = dungeon.getStart();
 
-            priorityQueue.add(start, 0);
-            start.markEnqueued();
+            priorityQueue.add(hex, 0);
+            hex.markEnqueued();
+
+            while (!priorityQueue.isEmpty() && !hex.equals(dungeon.getExit())) {
+                // Remove chamber with smallest priority and mark is as dequeued
+                hex = priorityQueue.removeMin();
+                hex.markDequeued();
+
+                boolean isNeighborDragon = false;
+                for (int i = 0; i < 5; i++) {
+                    if (hex.getNeighbour(i).isDragon()) {
+                        isNeighborDragon = true;
+                        break;
+                    }
+                }
+
+                if (hex.isDragon() || isNeighborDragon)
+                    continue;
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
